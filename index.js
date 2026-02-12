be7cb67b567d546170f8651febb75c24ecf24900
kihiku <gimdodam0520@gmail.com>
2026-02-12 10:06:10 +0900

    test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors()); // 크로스 오리진 설정
 app.use(express.json()); // 제이슨 데이터 파싱
-
+// test
 // 3. root 설정
 app.get('/', (request, response) => {
   response.send('This is the Main App for Deployment');
